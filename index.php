77ac7487ee16357e816bb8a5189dfa03d61c5adb
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-07-27 15:09:36 +0200

    add main structure

--- index.php
+++ index.php
@@ -3,7 +3,17 @@ Ci sono diverse domande con relative risposte.
 Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP. -->
 
 <?php 
+  $navBarLinks = ['Introduzione','Norme sulla privacy','Termini di servizio','Tecnologie','Domande frequenti'];
 
+  $qea = [
+    "Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?" => [
+      "La recente decisione della Corte di giustizia dell'Unione europea ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.",
+      "Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo lavorato incessantemente per uniformarci a essa. Si tratta di una procedura complessa perché dobbiamo valutare ogni singola richiesta e effettuare un bilanciamento tra il diritto dell'individuo a controllare i suoi dati personali ed il diritto di tutti di conoscere e distribuire le informazioni.",
+      "Per presentare una richiesta di rimozione, compila questo modulo web. Riceverai una risposta automatica che conferma la ricezione della tua richiesta. Dopodiché valuteremo il tuo caso (tieni presente che potrebbe occorrere un po' di tempo perché abbiamo già ricevuto tante richieste simili). Nel valutare la richiesta, considereremo se i risultati includono informazioni obsolete relative alla tua vita privata. Considereremo inoltre se vi è un interesse pubblico a che le informazioni rimangano nei nostri risultati delle ricerche, ad esempio se riguardano frodi finanziarie, negligenza professionale, condanne penali o la tua condotta pubblica in relazione a un pubblico ufficio (eletto o non eletto). Queste valutazioni sono complesse e, in quanto organizzazione privata, potremmo non essere nella posizione giusta per prendere decisioni in merito al tuo caso. Se non sei d'accordo con la nostra valutazione, puoi rivolgerti all'Autorità garante per la protezione dei dati personali nel tuo paese.",
+      "Nei prossimi mesi lavoreremo a stretto contatto con le autorità per la protezione dei dati e con altre autorità per perfezionare il nostro approccio. La decisione della CGUE rappresenta un cambiamento significativo per i motori di ricerca. Siamo preoccupati per le sue conseguenze, ma riteniamo anche che sia importante rispettare la decisione della Corte e ci stiamo adoperando per predisporre una procedura conforme alla legge.",
+      "Quando cerchi un nome, potresti trovare una notifica in cui viene spiegato che i risultati potrebbero essere stati modificati nel rispetto delle leggi europee per la protezione dei dati. Mostriamo questa notifica agli utenti europei quando cercano la maggior parte dei nomi, non soltanto le pagine che sono state soggette a una rimozione."
+    ]
+  ];
 ?>
 
 <!DOCTYPE html>
@@ -13,6 +23,12 @@ Gestire il “Database” e la visualizzazione di queste domande e risposte con 
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+  <!-- my style -->
+  <link rel="stylesheet" href="css/style.css">
+
+  <!-- fontawesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
@@ -20,20 +36,49 @@ Gestire il “Database” e la visualizzazione di queste domande e risposte con 
 </head>
 <body>
   <!-- inizio header -->
-  <header>
+  <header class="position-fixed top-0 start-0 end-0 bg-light">
+    <div class="top my-2 d-flex justify-content-between px-5">
+      <!-- inizio parte sinistra -->
+      <div>
+        <img id="logo" src="https://loghi-famosi.com/wp-content/uploads/2020/09/Google-Logo.png" alt="logo google">
+        <span class="text-secondary fs-4 align-middle">Privacy e termini</span>
+      </div>
+      <!-- fine parte sinistra -->
 
+      <!-- inizio parte destra -->
+      <div>
+        <i class="fas fa-th text-secondary"></i>
+        <div class="avatar ms-2">M</div>
+      </div>
+      <!-- fine parte destra -->
+    </div>
+    <div class="bottom border-bottom px-5">
+      <ul class="list-inline">
+        <?php foreach($navBarLinks as $link) {?>
+          <li class="list-inline-item me-5"><a href="#" class="text-decoration-none text-secondary"><?php echo $link; ?></a></li>
+        <?php } ?>
+      </ul>
+    </div>
   </header>
   <!-- fine header -->
 
   <!-- inizio main -->
-  <main>
-
+  <main class="mt-150">
+    <div class="ms-container text-wrap">
+      <?php foreach($qea as $question => $answers){ ?>
+        <h2 class="fs-4 fw-bold"><?php echo $question; ?></h2>
+        <?php foreach($answers as $answer){ ?>
+          <p><?php echo $answer; ?></p>
+          <!-- echo $answer . '<br>'; -->
+        <?php } ?>
+      <?php } ?>
+    </div>   
   </main>
   <!-- fine main -->
 
   <!-- inizio footer -->
   <footer>
-    
+
   </footer>
   <!-- fine footer -->
 
