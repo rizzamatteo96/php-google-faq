f4f91f8c62949c71e3c92fcf4bca89781daa7919
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-07-27 17:57:42 +0200

    add more qea in array

--- index.php
+++ index.php
@@ -19,6 +19,19 @@ Gestire il “Database” e la visualizzazione di queste domande e risposte con 
       "Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, nel Centro Google per la sicurezza online.",
       "Scopri quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo."
     ],
+    "Perché il mio account è associato a un paese?" => [
+      "Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:
+      <ol>
+        <li>La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:
+          <ol style='list-style-type: lower-latin;'>
+            <li>Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.</li>
+            <li>Google LLC, con sede negli Stati Uniti, per il resto del mondo.</li>
+          </ol>
+        </li>
+        <li>La versione dei termini che regola il nostro rapporto, che può variare in base alle leggi locali.</li>
+      </ol>
+      Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account."
+    ],
     "Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?" => [
       "I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi contattare il webmaster del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica. Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei risultati di ricerca di Google, fai clic qui. Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato l'aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente, è inoltre possibile visitare la nostra pagina di assistenza per avere ulteriori informazioni."
     ],
